a4c778a878269e1aa9dbbeb59e69e9fdf3b03ca1
DanVerh <92340480+DanVerh@users.noreply.github.com>
2022-02-18 12:28:58 +0200

    app installation added

--- postbuild.sh
+++ postbuild.sh
@@ -1,3 +1,7 @@
+sudo apt -y update
+sudo apt -y install libpq-dev python-dev
+pip install 'Jinja2<3'
+pip install .
 sudo mkdir /project1
 chmod 777 /project1
 trac-admin /project1 initenv trac sqlite:db/trac.db
