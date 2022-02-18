dbd3de230f702d9b23045d9e7b21f1f673598d90
DanVerh <92340480+DanVerh@users.noreply.github.com>
2022-02-18 16:51:53 +0200

    Update postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -3,5 +3,5 @@ pip install --upgrade pip
 pip install markupsafe==2.0.1
 pip install 'Jinja2<3'
 pip install .
-mkdir ~/project1
-trac-admin ~/project1 initenv trac sqlite:db/trac.db
+mkdir ~/project2
+trac-admin ~/project2 initenv trac sqlite:db/trac.db
