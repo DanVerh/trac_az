2989b8fd323b850d01d9e0fa8f1b37775e4dea7b
DanVerh <92340480+DanVerh@users.noreply.github.com>
2022-02-18 15:16:59 +0200

    Update postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -5,6 +5,7 @@ apt-get -y install sudo
 sudo apt -y update
 sudo apt -y install libpq-dev python-dev
 pip install --upgrade pip
+pip install markupsafe==2.0.1
 pip install 'Jinja2<3'
 pip install .
 sudo mkdir /project1
