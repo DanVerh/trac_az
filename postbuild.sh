a9ba0cac857b04930e543069ea8ac964aa852fbb
DanVerh <92340480+DanVerh@users.noreply.github.com>
2022-02-18 15:08:05 +0200

    Update postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+su -
 apt-get -y install sudo
 sudo apt -y update
 sudo apt -y install libpq-dev python-dev
 pip install --upgrade pip
 pip install 'Jinja2<3'
 pip install .
-mkdir /project1
+sudo mkdir /project1
 trac-admin /project1 initenv trac sqlite:db/trac.db
