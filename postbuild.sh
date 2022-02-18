12c98ad1efa4b17d903feecc52e7d197bb987f1e
DanVerh <92340480+DanVerh@users.noreply.github.com>
2022-02-18 12:37:22 +0200

    + sudo chmod

--- postbuild.sh
+++ postbuild.sh
@@ -3,5 +3,5 @@ sudo apt -y install libpq-dev python-dev
 pip install 'Jinja2<3'
 pip install .
 sudo mkdir /project1
-chmod 777 /project1
+sudo chmod 777 /project1
 trac-admin /project1 initenv trac sqlite:db/trac.db
